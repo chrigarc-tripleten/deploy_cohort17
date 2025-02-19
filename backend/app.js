53ba6df09db1c9c1bd1736ce32eb185d0ba3c878
christian garcia <christian.garcia@labbe.io>
2025-02-18 18:40:59 -0600

    mi primer backend

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/cohort17");
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.use(
+  cors({
+    origin: ["https://cohort17.desarrollointerno.com", "http://localhost:5173"],
+  })
+);
+
+app.get('/hello', (req, res) => {
+    res.send({
+        message: 'Hello world'
+    })
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}...`);
+});
